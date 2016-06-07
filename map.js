745b9c42517d037c58173c1f76186c3287d6700d
Chris Chao <chris_chao@trend.com.tw>
2016-06-07 16:31:25 +0800

    Change background to white

--- map.js
+++ map.js
@@ -9,42 +9,42 @@
  */
 
 Pacman.Map = function (size) {
-    
-    var height    = null, 
-        width     = null, 
+
+    var height    = null,
+        width     = null,
         blockSize = size,
         pillMin   = 8,
         pillMax   = 16,
         pillStep  = 0.4,
         pillSize  = pillMin,
         map       = null;
-    
+
     function withinBounds(y, x) {
         return y >= 0 && y < height && x >= 0 && x < width;
     }
-    
+
     function isWall(pos) {
         return withinBounds(pos.y, pos.x) && map[pos.y][pos.x] === Pacman.WALL;
     }
-    
+
     function isFloorSpace(pos) {
         if (!withinBounds(pos.y, pos.x)) {
             return false;
         }
         var peice = map[pos.y][pos.x];
-        return peice === Pacman.EMPTY || 
+        return peice === Pacman.EMPTY ||
             peice === Pacman.BISCUIT ||
             peice === Pacman.PILL;
     }
-    
+
     function drawWall(ctx) {
 
         var i, j, p, line;
-        
-        ctx.strokeStyle = "#0000FF";
+
+        ctx.strokeStyle = "#777";
         ctx.lineWidth   = 5;
         ctx.lineCap     = "round";
-        
+
         for (i = 0; i < Pacman.WALLS.length; i += 1) {
             line = Pacman.WALLS[i];
             ctx.beginPath();
@@ -52,50 +52,50 @@ Pacman.Map = function (size) {
             for (j = 0; j < line.length; j += 1) {
 
                 p = line[j];
-                
+
                 if (p.move) {
                     ctx.moveTo(p.move[0] * blockSize, p.move[1] * blockSize);
                 } else if (p.line) {
                     ctx.lineTo(p.line[0] * blockSize, p.line[1] * blockSize);
                 } else if (p.curve) {
-                    ctx.quadraticCurveTo(p.curve[0] * blockSize, 
+                    ctx.quadraticCurveTo(p.curve[0] * blockSize,
                                          p.curve[1] * blockSize,
-                                         p.curve[2] * blockSize, 
-                                         p.curve[3] * blockSize);   
+                                         p.curve[2] * blockSize,
+                                         p.curve[3] * blockSize);
                 }
             }
             ctx.stroke();
         }
     }
-    
-    function reset() {       
+
+    function reset() {
         map    = Pacman.MAP.clone();
         height = map.length;
-        width  = map[0].length;        
+        width  = map[0].length;
     };
 
     function block(pos) {
         return map[pos.y][pos.x];
     };
-    
+
     function setBlock(pos, type) {
         map[pos.y][pos.x] = type;
     };
 
-    function drawPills(ctx) { 
+    function drawPills(ctx) {
         var img = document.getElementById('tball');
 
         pillSize += pillStep;
         if ((pillSize > pillMax) || (pillSize < pillMin)) {
             pillStep = -pillStep;
         }
-        
+
         for (i = 0; i < height; i += 1) {
 		    for (j = 0; j < width; j += 1) {
                 if (map[i][j] === Pacman.PILL) {
                     ctx.beginPath();
-                    ctx.fillStyle = "#000";
-		            ctx.fillRect((j * blockSize), (i * blockSize), 
+                    ctx.fillStyle = "#FFF";
+		            ctx.fillRect((j * blockSize), (i * blockSize),
                                  blockSize, blockSize);
                     ctx.closePath();
 
@@ -109,23 +109,23 @@ Pacman.Map = function (size) {
 		    }
 	    }
     };
-    
+
     function draw(ctx) {
-        
+
         var i, j, size = blockSize;
 
-        ctx.fillStyle = "#000";
+        ctx.fillStyle = "#FFF";
 	    ctx.fillRect(0, 0, width * size, height * size);
 
         drawWall(ctx);
-        
+
         for (i = 0; i < height; i += 1) {
 		    for (j = 0; j < width; j += 1) {
 			    drawBlock(i, j, ctx);
 		    }
 	    }
     };
-    
+
     function drawBlock(y, x, ctx) {
 
         var layout = map[y][x];
@@ -135,26 +135,26 @@ Pacman.Map = function (size) {
         }
 
         ctx.beginPath();
-        
-        if (layout === Pacman.EMPTY || layout === Pacman.BLOCK || 
+
+        if (layout === Pacman.EMPTY || layout === Pacman.BLOCK ||
             layout === Pacman.BISCUIT) {
-            
-            ctx.fillStyle = "#000";
-		    ctx.fillRect((x * blockSize), (y * blockSize), 
+
+            ctx.fillStyle = "#FFF";
+		    ctx.fillRect((x * blockSize), (y * blockSize),
                          blockSize, blockSize);
 
             if (layout === Pacman.BISCUIT) {
-                ctx.fillStyle = "#FFF";
-		        ctx.fillRect((x * blockSize) + (blockSize / 2.5), 
-                             (y * blockSize) + (blockSize / 2.5), 
+                ctx.fillStyle = "#000";
+		        ctx.fillRect((x * blockSize) + (blockSize / 2.5),
+                             (y * blockSize) + (blockSize / 2.5),
                              blockSize / 6, blockSize / 6);
 	        }
         }
-        ctx.closePath();	 
+        ctx.closePath();
     };
 
     reset();
-    
+
     return {
         "draw"         : draw,
         "drawBlock"    : drawBlock,
