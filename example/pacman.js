6e1bdcc8b12a004bf8d37bf0be8fa2b8be817e9b
System Administrator <root@tw-williamsu_mbpr.client.tw.trendnet.org>
2016-06-07 16:39:33 +0800

    add eyes for footer Pacman

--- example/pacman.js
+++ example/pacman.js
@@ -924,6 +924,17 @@ var PACMAN = (function () {
                     (topLeft+1) + map.blockSize / 2,
                     map.blockSize / 2, Math.PI * 0.25, Math.PI * 1.75, false);
             ctx.fill();
+
+            // Draw eyes
+            ctx.fillStyle = "#dd363a";
+            ctx.beginPath();
+            ctx.moveTo(150 + (25 * i) + map.blockSize / 2,
+                (topLeft+1) + map.blockSize / 2);
+
+            ctx.arc((150 + (25 * i) + map.blockSize / 2) -3,
+                ((topLeft+1) + map.blockSize / 2) -3,
+                1.5, 0, Math.PI * 2, false);
+            ctx.fill();
         }
 
         ctx.fillStyle = !soundDisabled() ? "#00FF00" : "#FF0000";
